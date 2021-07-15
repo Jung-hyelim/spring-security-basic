03649b27083ce64c896c0a43a532d716a17e52f9
Jung-hyelim <jhl3646@gmail.com>
2021-07-15 15:51:38 +0900

    basic spring security example

---
+++ src/main/java/com/example/springsecuritybasic/AccountRunner.java
@@ -0,0 +1,19 @@
+package com.example.springsecuritybasic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountRunner implements ApplicationRunner {
+
+    @Autowired
+    AccountService accountService;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Account admin = accountService.createAccount("user", "1234");
+        System.out.println(admin.getUsername() + " / " + admin.getPassword());
+    }
+}
---
+++ src/test/java/com/example/springsecuritybasic/HelloControllerTest.java
@@ -0,0 +1,44 @@
+package com.example.springsecuritybasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("hello"));
+    }
+
+    @Test
+    public void my_without_user() throws Exception {
+        mockMvc.perform(get("/my"))
+                .andDo(print())
+                .andExpect(status().isUnauthorized())
+                ;
+    }
+
+    @Test
+    @WithMockUser
+    public void my_with_user() throws Exception {
+        mockMvc.perform(get("/my"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("my"));
+    }
+}
